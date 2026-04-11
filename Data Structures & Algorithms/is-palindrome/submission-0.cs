eabf44690840278c4c94b0442805acf574930b8e
IronPole9697 <41614369+vinhngogia0906@users.noreply.github.com>
2026-04-11 19:25:44 +1000

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsPalindrome(string s) {    
+        int l = 0, r = s.Length - 1;
+        while (l < r) {                                                                                                             while (l < r && !char.IsLetterOrDigit(s[l])) l++;
+            while (l < r && !char.IsLetterOrDigit(s[r])) r--;
+            if (char.ToLower(s[l]) != char.ToLower(s[r])) return false;
+              l++;
+              r--;
+        }
+        return true;
+    }
+}
